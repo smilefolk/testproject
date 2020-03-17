4155372199b823142b71bf00e9a5291059081448
Folk <folk@NBAAP1400010.local>
2020-03-17 14:32:51 +0700

    fixed

--- src/api/routes/v1/test.route.js
+++ src/api/routes/v1/test.route.js
@@ -2,9 +2,6 @@ const express = require('express');
 const validate = require('express-validation');
 const controller = require('../../controllers/test.controller');
 const { authorize, SUPER_ADMIN, PRODUCT_OWNER } = require('../../middlewares/auth');
-const {
-  listUsers,
-} = require('../../validations/banner.validation');
 
 const router = express.Router();
 router.route('/test').get(controller.test);
